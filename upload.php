2f3b202248c04eb2cf6fa82a3130099b6f962d00
Michelle Loh <michellelohwt0799@gmail.com>
2021-11-21 23:35:02 +0800

    remove error detection from upload.php

--- upload.php
+++ upload.php
@@ -2,39 +2,32 @@
 # Check if there is anything submitted through the form
 if (isset($_POST['submit'])){
 
-    # Get the information of the uploaded file
-    $fileName = $_FILES['upload_file']['name'];
-    $fileTmpName = $_FILES['upload_file']['tmp_name'];
-    $fileSize = $_FILES['upload_file']['size'];
-    $fileError = $_FILES['upload_file']['error'];
-    $fileType = $_FILES['upload_file']['type'];
+  # Get the information of the uploaded file
+  $fileName = $_FILES['upload_file']['name'];
+  $fileTmpName = $_FILES['upload_file']['tmp_name'];
+  $fileSize = $_FILES['upload_file']['size'];
 
-    # Get the name and extension of the file
-    $fileExt = explode(".", $fileName);
+  # Get the name and extension of the file
+  $fileExt = explode(".", $fileName);
 
-    # Turn the extension to lower case
-    $fileActualExt = end($fileExt);
+  # Turn the extension to lower case
+  $fileActualExt = end($fileExt);
 
-    if($fileActualExt == 'pdf'){
-        if($fileError == 0){
-            if ($fileSize < 1000000){
-//                # 1000000kb = 1000mb
-//                $fileNameNew = uniqid('', true)."."."$fileActualExt";  # create an unique id for the file to prevent overwritten by the same file name
-//                $fileDestination = 'uploads/'.$fileNameNew;
-//                move_uploaded_file($fileTmpName, $fileDestination);
-//                header("Location: index.php?uploadsuccess");
-                echo"upload success";
-            }
-            else{
-                echo "The file is too big!";
-            }
-        }
-        else{
-            echo "There was an error uploading your file!";
-        }
+  if($fileActualExt == 'pdf'){
+    if ($fileSize < 1000000){
+        # 1000000kb = 1000mb
+        $fileNameNew = uniqid('', true)."."."$fileActualExt";  # create an unique id for the file to prevent overwritten by the same file name
+        $fileDestination = 'uploads/'.$fileNameNew;
+        move_uploaded_file($fileTmpName, $fileDestination);
+        header("Location: index.php?uploadsuccess");
+        echo"upload success";
     }
-    else {
-        echo "Wrong file type! Please upload pdf.";
+    else{
+        echo "The file is too big!";
     }
+  }
+  else {
+      echo "Wrong file type! Please upload pdf.";
+  }
 }
 ?>
