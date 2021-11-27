4308d392f7a58579dd1dc3e767da10c1edb33117
Michelle Loh <michellelohwt0799@gmail.com>
2021-11-27 18:35:48 +0800

    add style.php for php styling

---
+++ css/style.php
@@ -0,0 +1,28 @@
+<?php
+header("Content-type: text/css; charset: UTF-8");
+?>
+
+.error{
+background: red;
+color: white;
+text-align: center;
+
+position: absolute;
+width: 500px;
+height: 30px;
+top: 33%;
+left: 45%;
+transform: translate(10%,10%);
+}
+
+.success{
+background: greenyellow;
+text-align: center;
+
+position: absolute;
+width: 500px;
+height: 30px;
+top: 33%;
+left: 45%;
+transform: translate(10%,10%);
+}
